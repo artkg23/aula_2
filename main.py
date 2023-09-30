3662c1f5708c2c81e9886fb13611f55ca13cb7a3
artkg23 <146586448+artkg23@users.noreply.github.com>
2023-09-30 17:52:29 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+# nome=input('digite seu nome')
+# idade=input('digite aqui sua idade')
+# meu_endereco=input('digite seu endereço')
+# cargo=input('digite aqui o seu cargo')
+
+# print(f'meu nome é{nome},minha idade é{idade},meu endereço é{meu_endereco},meu cargo é{cargo}')
+
+# frase=input('termine a frase, Só sei que...')
+
+# print(f'Só sei que  {frase}')
+
+# horas=input('que horas são?  ')
+# minutos=input('que minutos são?  ')
+# segundos=input('que segundos são?  ')
+# print(f'são-{horas} horas,é-{minutos} minutos,é-{segundos} segundos')
+
+# numero=input('coloque seu numero de telefone:')
+# codigo=input('coloque seu codigo:')
+# print(f'seu codigo é...{codigo}, e seu numero de telefone é...{numero}')
+
+
+# numero = float(input('digite um numero'))
+
+# if numero == 10:
+#   print(f'0 numero é {numero}')
+# else:
+#   print(f'opa é {numero}')
+# senha_digitada =input('digite a sua senha')
+# nome = input('digite seu nome:')
+# senha='1234'
+
+# if senha == senha_digitada and nome =='camila':
+#  print('acesse,vc digitou corretamente')
+# else:
+#  print(f'não pode acessar,pois vc digitou{senha},ou  {nome}  incorreto')
+
+# nota=10
+# if nota>=80:
+#  print('otimo')
+# elif nota>=75:
+#  print('melhorar')
+# else:
+#  print('reprovado(a) adanis')
+
+# valor=19
+# if valor>=500:
+#  print('podemos parcela')
+# elif valor>=350:
+#  print('valor incorreto')
+# else:
+#  print('valor indefinido')
+
+# age=int(input('qual sua idade'))
+# if  age>=18 and age <=65:
+#    print('você já está na idade de votar')
+# else:
+#      print('você não pode votar')
+numero=7
+if numero % 2==2:
+  print('0numero é par.')
+else:
+  print('o numero é impar')
